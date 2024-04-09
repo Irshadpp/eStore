b8b171f6c07dfc051316b3e49e8b31f2f8c07534
Irshad <irshad89434@gmail.com>
2024-04-09 13:07:45 +0530

    Fix: database problem fixed

--- database/connection.js
+++ database/connection.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async ()=>{
     try {
-        const conn = await mongoose.connect(process.env.DB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        const conn = await mongoose.connect(process.env.DB_URI);
     console.log("Database connected")
 }catch(error){
     console.log(error);
